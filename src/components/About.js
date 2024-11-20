62e5f74e14d42e161e822c8d7b63a371a310677d
mohamadabdelhady <mohamadhady010@gmail.com>
2024-11-20 22:51:30 +0200

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+const About = () => {
+    return (
+      <div>
+        <Header />
+        <h1>Welcome to the About Page!</h1>
+      </div>
+    );
+  };
+  
+  export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+const Home=()=>{
+    return (
+        <div>
+          <Header />
+          <h1>Welcome to the Home Page!</h1>
+        </div>
+      );
+}
+
+export default Home;
